f9a61a1fe3baa41dc45f243f04819b46b3da3416
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-13 21:18:15 +0530

    Update SumOfMiddleElementsOfTwoSortedArray.java

--- SumOfMiddleElementsOfTwoSortedArray.java
+++ SumOfMiddleElementsOfTwoSortedArray.java
@@ -1,11 +1,33 @@
+/*
+Given 2 sorted arrays Ar1 and Ar2 of size N each. Merge the given arrays and find the sum of the 
+two middle elements of the merged array.
+Input:
+N = 5
+Ar1[] = {1, 2, 4, 6, 10}
+Ar2[] = {4, 5, 6, 9, 12}
+Output: 11
+Explanation: The merged array looks like
+{1,2,4,4,5,6,6,9,10,12}. Sum of middle
+elements is 11 (5 + 6).
 
+Input:
+N = 5
+Ar1[] = {1, 12, 15, 26, 38}
+Ar2[] = {2, 13, 17, 30, 45}
+Output: 32
+Explanation: The merged array looks like
+{1, 2, 12, 13, 15, 17, 26, 30, 38, 45} 
+sum of middle elements is 32 (15 + 17)
+*/
 class SumOfMiddleElementsOfTwoSortedArray
 {
 	public static void main(String[] args) 
 	{
-		int arr1[] = {1, 2, 4, 6, 10};
+		//int arr1[] = {1, 2, 4, 6, 10};
+		//int arr2[] = {4, 5, 6, 9, 12};
 
-		int arr2[] = {4, 5, 6, 9, 12};
+		int arr1[] = {1, 12, 15, 26, 38};
+		int arr2[] = {2, 13, 17, 30, 45};
 
 		int newarr[] = new int[arr1.length+arr2.length];
 
