4646824b38a6f7ec76e0bcdf142e51e2cf1f7ae8
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-14 11:46:34 +0530

    Update FindElementThatAppearsOnceInSortedArray.java

--- FindElementThatAppearsOnceInSortedArray.java
+++ FindElementThatAppearsOnceInSortedArray.java
@@ -13,31 +13,37 @@ class FindElementThatAppearsOnceInSortedArray
 {
 	public static void main(String[] args) 
 	{
-		int arr[] = {1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65};
+		//int arr[] = {1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65};
+		int arr[] = {1, 1, 2, 2, 3, 3, 4, 8, 8, 8, 9, 50, 50, 65, 65,121,3234,3234,3235};
 		int flag=0;
 		int count=0;
 		for (int i=0;i<arr.length;i++) 
 		{
-			if(arr[i] != -1)
+			for (int j=i+1;j<arr.length;j++) 
 			{
-				for (int j=i+1;j<arr.length;j++) 
+				if(arr[i] != -111)
 				{
 					if(arr[i] == arr[j])
 					{
-						flag=1;
-						arr[j]=-1;
-						break;
-					}
+						arr[j]=-111;
+						count++;
+					}		
 				}
-				if (flag == 0) 
-				{
-					System.out.println(arr[i]);	
-				}
-				flag=0;
+			}
+			if(count > 0)
+			{
+				arr[i] =-111;
+			}	
+			count=0;
+		}
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i] != -111)
+			{
+				System.out.print(arr[i]+" ");
 			}
 		}
-
-
 	}
 
 }
