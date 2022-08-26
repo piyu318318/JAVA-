d7637f5266b267806e0c22cf8e2c2eaae8ce1245
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-26 22:07:05 +0530

    Create InheritanceInJava.java

---
+++ PreparationsProgram/InheritanceInJava.java
@@ -0,0 +1,38 @@
+
+
+interface Piyush
+{
+	int n=19;//in interface variables are default public static final
+	public void display();//in interface methods are default abstract
+}	
+
+interface Madhav
+{
+  	int n=10;
+	public void show();
+}
+
+class Demo implements Piyush,Madhav
+{
+	public void display()
+	{
+		System.out.println("value of n is :"+n);
+		System.out.println("piyush");
+	}
+
+	public void show()
+	{
+    System.out.println("value of n is :"+n);
+		System.out.println("madhav");
+	}
+}	
+
+class InheritanceInJava 
+{
+	public static void main(String[] args) 
+	{
+		Demo obj = new Demo();
+		obj.display();
+		obj.show();
+	}
+}
