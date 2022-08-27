0be770a3bb6ae68500458123872d530305e1f50d
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-27 23:42:21 +0530

    Create PalindromeStringUsingArrayList.java

---
+++ Collection/PalindromeStringUsingArrayList.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+class PalindromeStringUsingHashMap
+{
+	public static void main(String[] args)throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a String to check its palindrome or not :");
+		String str1 = br.readLine();
+		ArrayList <Character>a1 = new ArrayList<Character>();
+		ArrayList <Character>a2 = new ArrayList<Character>();
+
+		for (int i=str1.length()-1;i>=0;i--) 
+		{
+			char temp=str1.charAt(i);
+			a1.add(temp);	
+		}
+
+		for (int i=0;i<str1.length();i++) 
+		{
+			char temp=str1.charAt(i);
+			a2.add(temp);	
+		}
+
+
+		if(a1.equals(a2) == true)
+			System.out.println("Palindrome");
+		else
+			System.out.println("not a palindrome");
+	}
+}
