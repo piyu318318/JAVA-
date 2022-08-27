1109ac67f82a0d15b27be2f24da825bd69da39f7
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-27 11:00:14 +0530

    Update InheritanceInJava.java

--- PreparationsProgram/InheritanceInJava.java
+++ PreparationsProgram/InheritanceInJava.java
@@ -1,4 +1,8 @@
-
+//using interfaces we can achieve a multiple inheriance in java as multiple inheritance is not supported by the java
+//example if i create class piyush, class madhav in which i have intitialize n=10 and n=20 both class extennded by the class demo when we use n in demo class
+//the demo class will get confused which n to used. to remove this drawback java has interfaces 
+//default variables in interfaces are public static final
+//default variables in interfaces are public abstract 
 
 interface Piyush
 {
