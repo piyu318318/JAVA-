ee70bacd9614389e82294156ee12c3bd9c6ab094
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-27 16:55:04 +0530

    Create InnerClass.java

---
+++ PreparationsProgram/InnerClass.java
@@ -0,0 +1,27 @@
+
+//A class within a class is called as innnerclass in java.
+//In Java, it is also possible to nest classes (a class within a class). 
+/*The purpose of nested classes is to group classes that belong together, which makes your code more
+ readable and maintainable.*/
+
+class ABC
+{
+	int x=10;
+
+	class XYZ
+	{
+		int y=20;
+	}
+}
+
+
+class InnerClass
+{
+	public static void main(String[] args) 
+	{
+		ABC outer = new ABC();
+		ABC.XYZ inner = outer.new XYZ();
+
+		System.out.println(inner.y+outer.x);
+	}
+}
