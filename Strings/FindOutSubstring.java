1d22030142b0567308dedfb7d00d33efb95a010f
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-08-30 18:04:38 +0530

    Update FindOutSubstring.java

--- Strings/FindOutSubstring.java
+++ Strings/FindOutSubstring.java
@@ -1,13 +1,15 @@
 class FindOutSubstring
 {
+	static int occurenceofsubstring=0;
 	public static void main(String[] args) 
 	{
+
 		String str1 = "acabasabkasab";
 		String str2="ab";
 		int count=0;
 		int k=0;
 		int j=0;
-		for (int i=0;i<str1.length();i++) 
+		for( int i=0;i<str1.length();i++) 
 		{
 			k=i;
 			while(j<str2.length())
@@ -25,11 +27,14 @@ class FindOutSubstring
 
 			if(count == str2.length())
 			{
-				System.out.println("india");
+				occurenceofsubstring++;
 			}
 
 			count=0;
 			j=0;
 		}
+		System.out.println("String1 is "+str1);
+		System.out.println("String2 is "+str2);
+		System.out.println("substring found in string is :"+FindOutSubstring.occurenceofsubstring);
 	}	
 }
