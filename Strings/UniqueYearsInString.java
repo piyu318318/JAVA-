2ee65d647a5938c7cc8d801903853a1971e08bf3
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-09-08 00:52:55 +0530

    Create UniqueYearsInString.java

---
+++ Strings/UniqueYearsInString.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class UniqueYearsInString
+{
+	public static void main(String[] args)
+	{
+		Set <String> set1 = new HashSet<String>();
+
+		String str1="UN was established on 24-10-1945.India got freedom on 15-08-1947.";
+		
+		StringBuffer s1 = new StringBuffer(str1);
+		StringBuffer s2 = new StringBuffer();
+
+		int count=0;
+		int check=0;
+
+		for (int i=0;i<s1.length();i++) 
+		{
+
+			if(i == s1.length()-4)
+				break;
+
+			count =0;
+
+			if(Character.isDigit(s1.charAt(i)))
+			{
+				if(Character.isDigit(s1.charAt(i+1)) && Character.isDigit(s1.charAt(i+2)) && Character.isDigit(s1.charAt(i+3)))
+				{
+					s2.append(s1.charAt(i));
+					s2.append(s1.charAt(i+1));
+					s2.append(s1.charAt(i+2));
+					s2.append(s1.charAt(i+3));
+					count++;
+				}
+ 			}
+
+ 			if(count == 1 )
+				s2.append(' ');
+
+		}
+		//now convert stringbuffer into string
+
+		String str = s2.toString();
+
+		String strarr[] = str.split(" ");
+		
+		String temp;
+
+		for(int i=0;i<strarr.length;i++) 
+		{
+			temp = strarr[i];
+			set1.add(temp);
+		}
+
+		System.out.println(set1);
+	}
+}
