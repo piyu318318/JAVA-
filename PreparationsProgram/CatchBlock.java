fe8c4b1b9d3c191e5041ce18843f285da15daf92
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-09-17 13:00:19 +0530

    Create CatchBlock.java

---
+++ PreparationsProgram/CatchBlock.java
@@ -0,0 +1,31 @@
+//following will error generarte
+/*
+C:\java1\java remain concepts>javac CatchBlock.java
+CatchBlock.java:14: error: exception ArithmeticException has already been caught
+                        catch(ArithmeticException e)
+                        ^
+1 error
+
+C:\java1\java remain concepts>
+*/
+
+
+class CatchBlock
+{
+	public static void main(String args[])
+	{
+			try
+			{
+					int y= 10/0;
+			}
+			catch(Exception e)
+			{
+				System.out.println("catched in exception block");
+			}
+			catch(ArithmeticException e)
+			{
+				System.out.println("catched in arithmatic exception block");
+			}
+
+	}
+}
