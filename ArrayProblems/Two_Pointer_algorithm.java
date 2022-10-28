9630b9d1ab2e2500aef330cd1f03f3260de925c4
piyu318318 <100851980+piyu318318@users.noreply.github.com>
2022-10-28 11:44:50 +0530

    Create Two_Pointer_algorithm.java

---
+++ ArrayProblems/Two_Pointer_algorithm.java
@@ -0,0 +1,18 @@
+/*
+1)what is two pointer technique ?
+two pointer technique normaly use d for searching and it uses two pointers in one loop in given data structure.
+2)why do you must know it?
+in order to use two pointers most of the time data structure needs to be ordered in some way which helps us to reduce the time complexity from O(n^2) , O(n^3) or O(n)
+of just with one loop with two pointers and search each item just one time.so depending upon whether the input string / array is sorted or not two pointer method can take 
+O(n log n) time complexity or even better which is O(n)
+
+3)variants of two pointers
+opposite directional -one pointer starats from beginning and other pointers starts from end.
+equi directional - both pointers starts from beggining.
+
+*/
+
+class Two_Pointer_algorithm
+{
+  
+}
